7d04b39d1dd08be885682352660720d3673e540c
Luisa Perez <luisa.perezw3@gmail.com>
2023-08-04 17:49:50 +0100

    adding new, show and edit views and links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-#cleaning database
+puts 'Cleaning database'
+
 Restaurant.destroy_all
 
 10.times do
-  Restaurant.create(name: Faker::Restaurant.name,
-                    address: Faker::Address.street_address,
-                    phone_number: Faker::PhoneNumber,
-                    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
-                  )
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+    )
+    puts "Restaurant with id #{restaurant.id} has been created"
 end
+
+puts 'Finished!'
